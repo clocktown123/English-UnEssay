8906da56239e09c992542d29edacca50d6d2f892
Jay Clock <146383205+clocktown123@users.noreply.github.com>
2025-04-23 15:46:48 -0600

    Update Player.py

--- Player.py
+++ Player.py
@@ -6,16 +6,25 @@ class player:
         self.y = 300
         self.w = 50
         self.h = 50
+        self.vx = 0
+        self.vy = 0
     
     def move(self, keys_pressed):
         if keys_pressed[pygame.K_LEFT]:
-            self.x -= 5
-        if keys_pressed[pygame.K_RIGHT]:
-            self.x += 5
+            self.vx = -.2
+        elif keys_pressed[pygame.K_RIGHT]:
+            self.vx = .2
+        else:
+            self.vx = 0
         if keys_pressed[pygame.K_UP]:
-            self.y -= 5
-        if keys_pressed[pygame.K_DOWN]:
-            self.y += 5
+            self.vy = -.2
+        elif keys_pressed[pygame.K_DOWN]:
+            self.vy = .2
+        else:
+            self.vy = 0
+
+        self.x+=self.vx
+        self.y+=self.vy
 
     def draw(self, screen):
         pygame.draw.rect(screen, (20, 20, 200), (self.x, self.y, self.w, self.h))
