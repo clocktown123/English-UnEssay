b18eeea1d739de4258eb1ca228f5444269b63a34
Jay Clock <146383205+clocktown123@users.noreply.github.com>
2025-04-30 22:19:42 -0600

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,30 +1,28 @@
 import pygame
 
 class player:
-    def __init__(self):
-        self.x = 300
-        self.y = 300
-        self.w = 50
-        self.h = 50
+    def __init__(self, position, Size):
+        self.pos = pygame.Vector2(position)
+        self.size = pygame.Vector2(Size)
         self.vx = 0
         self.vy = 0
     
     def move(self, keys_pressed):
         if keys_pressed[pygame.K_LEFT]:
-            self.vx = -.2
+            self.vx = -.1
         elif keys_pressed[pygame.K_RIGHT]:
-            self.vx = .2
+            self.vx = .1
         else:
             self.vx = 0
         if keys_pressed[pygame.K_UP]:
-            self.vy = -.2
+            self.vy = -.1
         elif keys_pressed[pygame.K_DOWN]:
-            self.vy = .2
+            self.vy = .1
         else:
             self.vy = 0
 
-        self.x+=self.vx
-        self.y+=self.vy
+        self.pos.x+=self.vx
+        self.pos.y+=self.vy
 
     def draw(self, screen):
-        pygame.draw.rect(screen, (20, 20, 200), (self.x, self.y, self.w, self.h))
+        pygame.draw.rect(screen, (20, 20, 200), (self.pos, self.size))
